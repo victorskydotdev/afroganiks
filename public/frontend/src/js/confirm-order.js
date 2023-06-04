80290508cb7a9d0e20c9af1320ab17ca674f606e
techyVS <victorsky90@gmail.com>
2023-06-04 20:50:05 +0100

    added a contact form

--- public/frontend/src/js/confirm-order.js
+++ public/frontend/src/js/confirm-order.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const checkout = document.querySelector('.checkout-btn');
+	const orderForm = document.querySelector('.shipping-form');
+
 	const name = document.getElementById('customer-name').value;
+
 	const email = document.getElementById('email-address').value;
 
-	checkout.addEventListener('click', () => {
+	orderForm.addEventListener('submit', (e) => {
+		e.preventDefault();
+
 		const capturedName = name;
 		const capturedEmail = email;
 
@@ -15,3 +19,35 @@ document.addEventListener('DOMContentLoaded', () => {
 		window.location.href = '/success';
 	});
 });
+
+const productName = sessionStorage.getItem('productName');
+const productPrice = parseInt(sessionStorage.getItem('productPrice'));
+
+if (productPrice || productName) {
+	const priceInfo = document.getElementById('price');
+	const ProductNameInput = document.getElementById('name');
+
+	ProductNameInput.value = productName;
+	priceInfo.value = productPrice;
+} else {
+	console.log('nothing found in session storage');
+}
+
+//===== logic for payments =====//
+
+// initialize all the global variables on the soccess page
+const nameHeading = document.getElementById('customer-name');
+const productInfoWrap = document.getElementById('product-name');
+
+// fetch the session storage data again
+// fetch the sessionStorage data (name and email)
+const customerName = sessionStorage.getItem('storedName');
+const customerEmail = sessionStorage.getItem('storedEmail');
+const productInfo = sessionStorage.getItem('productName');
+
+if (!(productInfoWrap || nameHeading)) {
+	console.log('nothing');
+} else {
+	nameHeading.textContent = customerName;
+	// productInfoWrap.textContent = productInfo;
+}
