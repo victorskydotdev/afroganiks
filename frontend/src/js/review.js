c9bff66dd0f2bb8e044bbb141907a9bb9aacd57d
techyVS <victorsky90@gmail.com>
2024-02-12 14:20:17 +0100

    turned off the review page

--- frontend/src/js/review.js
+++ frontend/src/js/review.js
@@ -1,1 +1,11 @@
 const reviewImage = document.querySelectorAll('.review-image');
+
+const blowupImage = () => {
+	for (let i = 0; i < reviewImage.length; i++) {
+		reviewImage[i].addEventListener('click', () => {
+			console.log('image clicked');
+		});
+	}
+};
+
+blowupImage();
